11f28bad74fb43b8cc0acbfa01c3b09fbbf72337
Maksim <maksimus.tumannick@yandex.ru>
2022-06-24 23:30:24 +0300

    Task2.10

---
+++ Task2.10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int maxValue = 100;
+            int numberFromRandom = random.Next(maxValue);
+            int degree = 0;
+            int numberInDegree = 0;
+
+            for (; numberInDegree <= numberFromRandom; degree++)
+            {
+                numberInDegree = (int)Math.Pow(2, degree);
+            }
+        }
+    }
+}
